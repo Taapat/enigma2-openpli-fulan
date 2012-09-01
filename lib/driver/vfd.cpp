8440f8bea7e54132f2f95eeeee26066312b7c4ab
andreyua <andrey.wys@gmail.com>
2012-09-01 22:37:19 +0300

    fix clear vfd

--- lib/driver/vfd.cpp
+++ lib/driver/vfd.cpp
@@ -187,7 +187,7 @@ void * start_loop (void *arg)
 	evfd vfd;
 	blocked = true;
 	//vfd.vfd_clear_icons();
-	vfd.vfd_write_string("Open AR-P ENIGMA2", true);
+	vfd.vfd_write_string("Open AR-P ENIGMA2");
 	//run 2 times through all icons 
 	for  (int vloop = 0; vloop < 128; vloop++) {
 #if !defined(PLATFORM_FORTIS_HDBOX) && !defined(PLATFORM_OCTAGON1008) && !defined(PLATFORM_ATEVIO7500) && !defined(PLATFORM_CUBEREVO) && !defined(PLATFORM_CUBEREVO_MINI) && !defined(PLATFORM_CUBEREVO_MINI2) && !defined(PLATFORM_CUBEREVO_MINI_FTA) && !defined(PLATFORM_CUBEREVO_250HD) && !defined(PLATFORM_CUBEREVO_2000HD) && !defined(PLATFORM_CUBEREVO_9500HD) && !defined(PLATFORM_HS7810A)
@@ -407,7 +407,13 @@ void evfd::vfd_write_string_scrollText(char* text) {
 #endif
 void evfd::vfd_clear_string()
 {
+#if defined(SPARK) || defined(SPARK7162)
+	file_vfd = open (VFD_DEVICE, O_WRONLY);
+	ioctl ( file_vfd, VFDDISPLAYCLR, &data );
+	close (file_vfd);
+#else
 	vfd_write_string("                ");
+#endif
 	return;
 }
 
