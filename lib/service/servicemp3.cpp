351c0d0f880177566f40da68f9a27f69b5b93674
Taapat <taapat@gmail.com>
2012-11-15 20:57:10 +0200

    fix2 rewind in some avi and mpg files

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -1059,14 +1059,19 @@ RESULT eServiceMP3::seekRelative(int direction, pts_t to)
 #ifndef ENABLE_LIBEPLAYER3
 	if (!m_gst_playbin)
 		return -1;
-#endif
+
 	pts_t ppos;
 	if (getPlayPosition(ppos) < 0) return -1;
 	ppos += to * direction;
 	if (ppos < 0)
 		ppos = 0;
 	return seekTo(ppos);
-}
+#else
+	float pos = direction * to / 90000.0;
+	if (player && player->playback)
+		player->playback->Command(player, PLAYBACK_SEEK, (void*)&pos);
+	return 0;
+#endif}
 
 #ifndef ENABLE_LIBEPLAYER3
 gint eServiceMP3::match_sinktype(GstElement *element, gpointer type)
