97e0754d7deedc616830af9bf4b07b7a5249cdd7
AndreyUA <andrey.wys@gmail.com>
2012-11-27 19:08:09 +0200

    [modemsetting]: add option debug and  internet connection sharing

--- lib/python/Plugins/Extensions/ModemSettings/plugin.py
+++ lib/python/Plugins/Extensions/ModemSettings/plugin.py
@@ -44,6 +44,8 @@ class ModemSetup(Screen, ConfigListScreen):
 			self.MODEMPPPDOPTS = settings.readline().strip()[15:-1]
 			self.DIALNUMBER = settings.readline().strip()[11:]
 			self.MODEMAUTOSTART = settings.readline().strip()[15:16]
+			self.DEBUG = settings.readline().strip()[6:7]
+			self.SHARE = settings.readline().strip()[6:7]
 			settings.close()
 		else:
 			self.MODEMTYPE = "0"
@@ -57,6 +59,8 @@ class ModemSetup(Screen, ConfigListScreen):
 			self.MODEMPPPDOPTS = ""
 			self.DIALNUMBER = '"*99#"'
 			self.MODEMAUTOSTART = "1"
+			self.DEBUG = "0"
+			self.SHARE = "0"
 		
 		self.MODEMTYPEEntry = ConfigSelection(default = self.MODEMTYPE, choices = [ ("0", _("gprs")), ("1", _("cdma")) ])
 		self.MODEMPORTEntry = ConfigSelection(default = self.MODEMPORT, choices = [ ("auto", _("auto")), ("ttyACM0", _("ttyACM0")), ("ttyUSB0", _("ttyUSB0")), ("ttyUSB1", _("ttyUSB1")), ("ttyUSB2", _("ttyUSB2")), ("ttyUSB3", _("ttyUSB3")), ("ttyUSB4", _("ttyUSB4")), ("ttyUSB5", _("ttyUSB5")), ("ttyUSB6", _("ttyUSB6")), ("ttyUSB7", _("ttyUSB7")), ("ttyUSB8", _("ttyUSB8")), ("ttyUSB9", _("ttyUSB9")) ])
@@ -69,6 +73,8 @@ class ModemSetup(Screen, ConfigListScreen):
 		self.MODEMPPPDOPTSEntry = ConfigText(default = self.MODEMPPPDOPTS, visible_width = 100, fixed_size = False)
 		self.DIALNUMBEREntry = ConfigSelection(default = self.DIALNUMBER, choices = [ ('"*99#"', _("*99#")), ('"*99***1#"', _("*99***1#")), ('"*99**1*1#"', _("*99**1*1#")), ('"#777"', _("#777")) ])
 		self.MODEMAUTOSTARTEntry = ConfigSelection(default = self.MODEMAUTOSTART, choices = [ ("1", _("yes")), ("0", _("no")) ])
+		self.DEBUGEntry = ConfigSelection(default = self.DEBUG, choices = [ ("1", _("yes")), ("0", _("no")) ])
+		self.SHAREEntry = ConfigSelection(default = self.SHARE, choices = [ ("1", _("yes")), ("0", _("no")) ])
 
 	def createSetup(self):
 		self.list = []
@@ -83,6 +89,8 @@ class ModemSetup(Screen, ConfigListScreen):
 		self.list.append(getConfigListEntry(_("Additional PPPD options"), self.MODEMPPPDOPTSEntry))
 		self.list.append(getConfigListEntry(_("Dial number"), self.DIALNUMBEREntry))
 		self.list.append(getConfigListEntry(_("Auto start"), self.MODEMAUTOSTARTEntry))
+		self.list.append(getConfigListEntry(_("Enable debug"), self.DEBUGEntry))
+		self.list.append(getConfigListEntry(_("Internet connection sharing"), self.SHAREEntry))
 		self["config"].list = self.list
 
 	def ok(self):
@@ -96,7 +104,7 @@ class ModemSetup(Screen, ConfigListScreen):
 		if (ret == True):
 			self.mbox = self.session.open(MessageBox, _("Configuration saved!"), MessageBox.TYPE_INFO, timeout = 3 )
 			settings = open("/etc/modem.conf", "w")
-			settings.write('MODEMTYPE=%s\nMODEMPORT=%s\nMODEMSPEED=%s\nAPN="%s"\nMODEMUSERNAME="%s"\nMODEMPASSWORD="%s"\nMODEMMTU=%s\nMODEMMRU=%s\nMODEMPPPDOPTS="%s"\nDIALNUMBER=%s\nMODEMAUTOSTART=%s' % (self.MODEMTYPEEntry.value, self.MODEMPORTEntry.value, self.MODEMSPEEDEntry.value, self.APNEntry.value, self.MODEMUSERNAMEEntry.value, self.MODEMPASSWORDEntry.value, self.MODEMMTUEntry.value, self.MODEMMRUEntry.value, self.MODEMPPPDOPTSEntry.value, self.DIALNUMBEREntry.value, self.MODEMAUTOSTARTEntry.value))
+			settings.write('MODEMTYPE=%s\nMODEMPORT=%s\nMODEMSPEED=%s\nAPN="%s"\nMODEMUSERNAME="%s"\nMODEMPASSWORD="%s"\nMODEMMTU=%s\nMODEMMRU=%s\nMODEMPPPDOPTS="%s"\nDIALNUMBER=%s\nMODEMAUTOSTART=%s\nDEBUG=%s\nSHARE=%s' % (self.MODEMTYPEEntry.value, self.MODEMPORTEntry.value, self.MODEMSPEEDEntry.value, self.APNEntry.value, self.MODEMUSERNAMEEntry.value, self.MODEMPASSWORDEntry.value, self.MODEMMTUEntry.value, self.MODEMMRUEntry.value, self.MODEMPPPDOPTSEntry.value, self.DIALNUMBEREntry.value, self.MODEMAUTOSTARTEntry.value, self.DEBUGEntry.value, self.SHAREEntry.value))
 			settings.close()
 			self.close()
 
