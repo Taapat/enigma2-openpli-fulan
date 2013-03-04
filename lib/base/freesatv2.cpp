814761c4153a328f8f251b2c7aad6e4e703d713c
Taapat <taapat@gmail.com>
2013-03-04 20:49:46 +0200

    freesatv2 fix compilation error, thx technic

--- lib/base/freesatv2.cpp
+++ lib/base/freesatv2.cpp
@@ -84,22 +84,21 @@ freesatHuffmanDecoder::~freesatHuffmanDecoder()
 */
 static unsigned char resolveChar(const char *str)
 {
-	const unsigned char* p = (const unsigned char*) str;
+	const char*p = str;
 	unsigned c0 = *p++, c1 = *p++;
-	const char *sp = (const char*) p;
 	if (c1)
 		switch(c0|c1<<8)
 		{
 			case '0'|'x'<<8:
-				if ( sscanf(sp,"%02x", &c1) == 1 )
+				if ( sscanf(p,"%02x", &c1) == 1 )
 					c0 = c1;
 				break;
 			case 'E'|'S'<<8:
-				if ( !strcmp(sp,"CAPE") )
+				if ( !strcmp(p,"CAPE") )
 					c0 = ESCAPE;
 				break;
 			case 'S'|'T'<<8:
-				if ( !strcmp(sp,"OP") )
+				if ( !strcmp(p,"OP") )
 					c0 = STOP;
 				else if ( !strcmp(sp,"ART") )
 					c0 = START;
