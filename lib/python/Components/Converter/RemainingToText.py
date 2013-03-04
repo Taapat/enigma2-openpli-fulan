e9d4c2bac083e8428a2f8980bb29f4661a80d973
Taapat <taapat@gmail.com>
2013-03-04 20:55:11 +0200

    RemainingToText completly remove FOLLOW

--- lib/python/Components/Converter/RemainingToText.py
+++ lib/python/Components/Converter/RemainingToText.py
@@ -9,10 +9,6 @@ class RemainingToText(Converter, object):
 	PROGRESS = 4
 	WITH_SECONDSPROGRESS = 5
 
-#+++>
-	FOLLOW = 6
-
-
 	def __init__(self, type):
 		Converter.__init__(self, type)
 		if type == "WithSeconds":
@@ -25,10 +21,6 @@ class RemainingToText(Converter, object):
 			self.type = self.PROGRESS
 		elif type == "WithSecondsProgress":
 			self.type = self.WITH_SECONDSPROGRESS
-#+++>
-		elif type == "FOLLOW":
-			self.type = self.FOLLOW	
-#+++<
 		else:
 			self.type = self.DEFAULT
 
