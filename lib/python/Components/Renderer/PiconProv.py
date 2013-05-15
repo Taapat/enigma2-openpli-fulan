4611b5d8982f545b66638a240529a60c5b0eb47a
Taapat <taapat@gmail.com>
2013-05-15 21:03:36 +0300

    PiconProv: merge code with Picon from openpli

--- lib/python/Components/Renderer/PiconProv.py
+++ lib/python/Components/Renderer/PiconProv.py
@@ -1,6 +1,7 @@
 from Renderer import Renderer
-from Tools.Directories import fileExists, pathExists, SCOPE_SKIN_IMAGE, \
-	SCOPE_CURRENT_SKIN, resolveFilename 
+from Tools.Alternatives import GetWithAlternative
+from Tools.Directories import pathExists, SCOPE_SKIN_IMAGE, \
+	SCOPE_CURRENT_SKIN, resolveFilename
 
 from enigma import ePixmap
 from os import path, listdir
@@ -27,30 +28,50 @@ def onMountpointAdded(mountpoint):
 		print "[PiconProv] Failed to investigate %s:" % mountpoint, ex
 
 def findPicon(serviceName):
-	for path in searchPaths:
-		if pathExists(path):
-			pngname = path + serviceName + ".png"
+	for piconPath in searchPaths:
+		if pathExists(piconPath):
+			pngname = piconPath + serviceName + ".png"
 			if pathExists(pngname):
 				return pngname
 	return ""
 
-class PiconProv(Renderer):
-	__module__ = __name__
+def getPiconName(serviceName):
+	if serviceName == "Unknown":
+		return ""
+	sname = serviceName.upper()
+	pngname = findPicon(sname)
+	return pngname
 
+class PiconProv(Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
-		self.path = 'piconProv'
-		self.nameCache = {}
-		self.pngname = ''
+		self.pngname = ""
+		self.lastPath = None
+		pngname = findPicon("picon_default")
+		self.defaultpngname = None
+		if not pngname:
+			tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
+			if pathExists(tmp):
+				pngname = tmp
+			else:
+				pngname = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/picon_default.png")
+		if path.getsize(pngname):
+			self.defaultpngname = pngname
+
+	def addPath(self, value):
+		if pathExists(value):
+			global searchPaths
+			if not value.endswith('/'):
+				value += '/'
+			if value not in searchPaths:
+				searchPaths.append(value)
 
 	def applySkin(self, desktop, parent):
-		attribs = []
-		for (attrib, value,) in self.skinAttributes:
-			if (attrib == 'path'):
-				self.path = value
-			else:
-				attribs.append((attrib,
-				 value))
+		attribs = self.skinAttributes[:]
+		for (attrib, value) in self.skinAttributes:
+			if attrib == "path":
+				self.addPath(value)
+				attribs.remove((attrib,value))
 		self.skinAttributes = attribs
 		return Renderer.applySkin(self, desktop, parent)
 
@@ -58,28 +79,18 @@ class PiconProv(Renderer):
 
 	def changed(self, what):
 		if self.instance:
-			pngname = ''
-			if (what[0] != self.CHANGED_CLEAR):
-				sname = self.source.text
-				sname = sname.upper()
-				pngname = self.nameCache.get(sname, '')
-				if (pngname == ''):
-					pngname = findPicon(sname)
-					if (pngname != ''):
-						self.nameCache[sname] = pngname
-			if (pngname == ''):
-				pngname = self.nameCache.get('default', '')
-				if (pngname == ''):
-					pngname = findPicon('picon_default')
-					if (pngname == ''):
-						tmp = resolveFilename(SCOPE_CURRENT_SKIN, 'picon_default.png')
-						if fileExists(tmp):
-							pngname = tmp
-						else:
-							pngname = resolveFilename(SCOPE_SKIN_IMAGE, 'skin_default/picon_default.png')
-					self.nameCache['default'] = pngname
-			if (self.pngname != pngname):
+			pngname = ""
+			if what[0] != self.CHANGED_CLEAR:
+				pngname = getPiconName(self.source.text)
+			if not pngname: # no picon for provider found
+				pngname = self.defaultpngname
+			if self.pngname != pngname:
+				if pngname:
+					self.instance.setScale(1)
+					self.instance.setPixmapFromFile(pngname)
+					self.instance.show()
+				else:
+					self.instance.hide()
 				self.pngname = pngname
-				self.instance.setPixmapFromFile(self.pngname)
 
 initPiconPaths()
