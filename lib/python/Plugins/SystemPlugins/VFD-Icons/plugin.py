71cd0c54e0a2cf88174f69bad596e22c3439128b
Taapat <taapat@gmail.com>
2013-05-15 18:31:38 +0300

    Plugin VFD-Icons: change to own, without settings

--- lib/python/Plugins/SystemPlugins/VFD-Icons/plugin.py
+++ lib/python/Plugins/SystemPlugins/VFD-Icons/plugin.py
@@ -1,93 +1,30 @@
 # -*- coding: utf-8 -*-
-#
-# by Taapat <taapat@gmail.com> 29-10-2012
-# review by <alexeytech@gmail.com> aka technic
+# by Taapat <taapat@gmail.com> 01-01-2013
 
-from Components.ActionMap import ActionMap
-from Components.config import config, ConfigSubsection, getConfigListEntry, ConfigSelection
-from Components.ConfigList import ConfigListScreen
 from Components.ServiceEventTracker import ServiceEventTracker
-from Components.Sources.StaticText import StaticText
-from enigma import iPlayableService, eTimer, evfd
 from Plugins.Plugin import PluginDescriptor
-from Screens.Screen import Screen
+
+from enigma import iPlayableService, evfd
 from ServiceReference import ServiceReference
-from time import localtime, strftime, sleep
-
-config.plugins.vfdicon = ConfigSubsection()
-config.plugins.vfdicon.displayshow = ConfigSelection(default = "channel", choices = [
-		("channel", _("channel")), ("channel number", _("channel number")), ("clock", _("clock")), ("blank", _("blank")) ])
-
-class ConfigVFDDisplay(Screen, ConfigListScreen):
-	def __init__(self, session):
-		Screen.__init__(self, session)
-		self.skinName = ["Setup"]
-		self.setTitle(_("VFD display configuration"))
-		self["key_red"] = StaticText(_("Cancel"))
-		self["key_green"] = StaticText(_("OK"))
-		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
-			{
-				"cancel": self.cancel,
-				"ok": self.keySave,
-				"green": self.keySave,
-				"red": self.cancel,
-			}, -2)
-		cfglist = []
-		cfglist.append(getConfigListEntry(_("Show on VFD display"), config.plugins.vfdicon.displayshow))
-		ConfigListScreen.__init__(self, cfglist)
-
-	def cancel(self):
-		main(self)
-		ConfigListScreen.keyCancel(self)
-
-	def keySave(self):
-		main(self)
-		ConfigListScreen.keySave(self)
-
-def opencfg(session, **kwargs):
-		session.open(ConfigVFDDisplay)
-		evfd.getInstance().vfd_write_string( "VFD SETUP" )
-
-def VFDdisplay(menuid, **kwargs):
-	if menuid == "system":
-		return [(_("VFD Display"), opencfg, "vfd_display", 44)]
-	else:
-		return []
 
 class VFDIcons:
 	def __init__(self, session):
 		self.session = session
-		self.timer = eTimer()
-		self.timer.callback.append(self.timerEvent)
 		self.onClose = [ ]
-		self.__event_tracker = ServiceEventTracker(screen = self,eventmap =
-			{
-				iPlayableService.evStart: self.WriteName,
-			})
+		self.__event_tracker = ServiceEventTracker(screen = self,
+			eventmap = {iPlayableService.evStart: self.WriteName})
 
 	def WriteName(self):
-		if config.plugins.vfdicon.displayshow.value != "clock":
+		service = self.session.nav.getCurrentlyPlayingServiceOrGroup()
+		if service:
+			path = service.getPath()
+			if path:
+				servicename = "PLAY"
+			else:
+				servicename = str(service.getChannelNum())
+		else:
 			servicename = "    "
-			if config.plugins.vfdicon.displayshow.value != "blank":
-				service = self.session.nav.getCurrentlyPlayingServiceOrGroup()
-				if service:
-					path = service.getPath()
-					if path:
-						servicename = "PLAY"
-					else:
-						if config.plugins.vfdicon.displayshow.value == "channel number":
-							servicename = str(service.getChannelNum())
-						else:
-							servicename = ServiceReference(service).getServiceName()
-			print "[VFD Display] text ", servicename[0:20]
-			evfd.getInstance().vfd_write_string(servicename[0:20])
-
-	def timerEvent(self):
-		if config.plugins.vfdicon.displayshow.value == "clock":
-			tm = localtime()
-			servicename = strftime("%H%M", tm)
-			evfd.getInstance().vfd_write_string(servicename[0:4])
-			self.timer.startLongTimer(60-tm.tm_sec)
+		evfd.getInstance().vfd_write_string(servicename)
 
 VFDIconsInstance = None
 
@@ -95,13 +32,8 @@ def main(session, **kwargs):
 	global VFDIconsInstance
 	if VFDIconsInstance is None:
 		VFDIconsInstance = VFDIcons(session)
-	if config.plugins.vfdicon.displayshow.value == "clock":
-		sleep(1)
-		VFDIconsInstance.timerEvent()
-	else:
-		VFDIconsInstance.WriteName()
+	VFDIconsInstance.WriteName()
 
 def Plugins(**kwargs):
-	return [
-	PluginDescriptor(name = _("VFD Display"), description = _("VFD display config"), where = PluginDescriptor.WHERE_MENU, fnc = VFDdisplay),
-	PluginDescriptor(where = PluginDescriptor.WHERE_SESSIONSTART, fnc = main ) ]
+	return [PluginDescriptor(where = PluginDescriptor.WHERE_SESSIONSTART,
+		fnc = main)]
