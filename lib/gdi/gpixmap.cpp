085e823d9817f3ff9b6416c8fea9ae8c0c9e9b68
Taapat <taapat@gmail.com>
2013-11-22 01:27:05 +0200

    gpixmap: enable accelAlloc only if accel, to disable accel blit

--- lib/gdi/gpixmap.cpp
+++ lib/gdi/gpixmap.cpp
@@ -114,7 +114,7 @@ static void removed_pixmap(int size)
 static inline void added_pixmap(int size) {}
 static inline void removed_pixmap(int size) {}
 #endif
-
+#if not defined(__sh__)
 static bool is_a_candidate_for_accel(const gUnmanagedSurface* surface)
 {
 	if (surface->stride < 48)
@@ -129,12 +129,16 @@ static bool is_a_candidate_for_accel(const gUnmanagedSurface* surface)
 			return false;
 	}
 }
-
+#endif
 gSurface::gSurface(int width, int height, int _bpp, int accel):
 	gUnmanagedSurface(width, height, _bpp)
 {
+#if defined(__sh__)
+	if (accel)
+#else
 	if ((accel > gPixmap::accelAuto) ||
 		((accel == gPixmap::accelAuto) && (is_a_candidate_for_accel(this))))
+#endif
 	{
 		if (gAccel::getInstance()->accelAlloc(this) != 0)
 				eDebug("ERROR: accelAlloc failed");
