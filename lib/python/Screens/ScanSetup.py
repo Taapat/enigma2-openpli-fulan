f382103ca97650ee6de0278dcd218254e1ce6f4c
Taapat <taapat@gmail.com>
2013-11-24 23:22:36 +0200

    ScanSetup: fix error in scan with predefined_transponder, thx vvv2

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -898,7 +898,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 					tps = nimmanager.getTransponders(orbpos)
 					if len(tps) and len(tps) > self.preDefTransponders.index:
 						tp = tps[self.preDefTransponders.index]
-						self.addSatTransponder(tlist, tp[1] / 1000, tp[2] / 1000, tp[3], tp[4], tp[7], orbpos, tp[5], tp[6], tp[8], tp[9])
+						self.addSatTransponder(tlist, tp[1] / 1000, tp[2] / 1000, tp[3], tp[4], tp[7], orbpos, tp[5], tp[6], tp[8], tp[9], tp[10], tp[11], tp[12])
 				removeAll = False
 			elif self.scan_type.value == "single_satellite":
 				sat = self.satList[index_to_scan][self.scan_satselection[index_to_scan].index]
