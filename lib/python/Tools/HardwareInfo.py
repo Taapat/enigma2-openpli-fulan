e6f46e18c9c0bbca898237efb82db833c4802a06
Taapat <taapat@gmail.com>
2014-03-01 22:19:20 +0200

    HardwareInfo: disable checks that does not exist on spark
    therefore disable hw_info.cfg too

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -8,7 +8,7 @@ class HardwareInfo:
 	device_model = None
 	device_version = ""
 	device_revision = ""
-	device_hdmi = False
+	device_hdmi = True
 
 	def __init__(self):
                 global hw_info
@@ -19,12 +19,14 @@ class HardwareInfo:
 
 		print "Scanning hardware info"
 		# Version
-		if os.path.exists("/proc/stb/info/version"):
-			self.device_version = open("/proc/stb/info/version").read().strip()
+		# Disable on spark because there is only a /proc/stb/info/model
+		#if os.path.exists("/proc/stb/info/version"):
+			#self.device_version = open("/proc/stb/info/version").read().strip()
 
 		# Revision
-		if os.path.exists("/proc/stb/info/board_revision"):
-			self.device_revision = open("/proc/stb/info/board_revision").read().strip()
+		# Disable on spark because there is only a /proc/stb/info/model
+		#if os.path.exists("/proc/stb/info/board_revision"):
+			#self.device_revision = open("/proc/stb/info/board_revision").read().strip()
 
 		# Name ... bit odd, but history prevails
 		if os.path.exists("/proc/stb/info/model"):
@@ -47,26 +49,28 @@ class HardwareInfo:
 				pass
 
 		# Model
-		for line in open((resolveFilename(SCOPE_SKIN, 'hw_info/hw_info.cfg')), 'r'):
-			if not line.startswith('#') and not line.isspace():
-				l = line.strip().replace('\t', ' ')
-				if l.find(' ') != -1:
-					infoFname, prefix = l.split()
-				else:
-					infoFname = l
-					prefix = ""
-				if os.path.exists("/proc/stb/info/" + infoFname):
-					self.device_model = prefix + open("/proc/stb/info/" + infoFname).read().strip()
-					break
+		# Disable on spark because there is only a /proc/stb/info/model
+		#for line in open((resolveFilename(SCOPE_SKIN, 'hw_info/hw_info.cfg')), 'r'):
+			#if not line.startswith('#') and not line.isspace():
+				#l = line.strip().replace('\t', ' ')
+				#if l.find(' ') != -1:
+					#infoFname, prefix = l.split()
+				#else:
+					#infoFname = l
+					#prefix = ""
+				#if os.path.exists("/proc/stb/info/" + infoFname):
+					#self.device_model = prefix + open("/proc/stb/info/" + infoFname).read().strip()
+					#break
 
 		if self.device_model is None:
 			self.device_model = self.device_name
 
 		# HDMI capbility
-		self.device_hdmi = (	self.device_name == 'dm7020hd' or
-					self.device_name == 'dm800se' or
-					self.device_name == 'dm500hd' or
-					(self.device_name == 'dm8000' and self.device_version != None))
+		# Disable on spark
+		#self.device_hdmi = (	self.device_name == 'dm7020hd' or
+					#self.device_name == 'dm800se' or
+					#self.device_name == 'dm500hd' or
+					#(self.device_name == 'dm8000' and self.device_version != None))
 
 		print "Detected: " + self.get_device_string()
 
