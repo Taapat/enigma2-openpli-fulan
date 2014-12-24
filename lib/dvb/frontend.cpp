6e3735a2ffe12ca15952f68356e0c84c73e78145
Taapat <taapat@gmail.com>
2014-12-24 10:46:27 +0200

    Disable DVBT2 on sh, looks like its not suported in kernel headers from stlinux

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1819,7 +1819,9 @@ void eDVBFrontend::setFrontend(bool recvEvents)
 			{
 				default:
 				case eDVBFrontendParametersTerrestrial::System_DVB_T: system = SYS_DVBT; break;
+#if not defined(__sh__)
 				case eDVBFrontendParametersTerrestrial::System_DVB_T2: system = SYS_DVBT2; break;
+#endif
 			}
 
 			p[cmdseq.num].cmd = DTV_DELIVERY_SYSTEM, p[cmdseq.num].u.data = system, cmdseq.num++;
@@ -1923,6 +1925,7 @@ void eDVBFrontend::setFrontend(bool recvEvents)
 			cmdseq.num++;
 
 			p[cmdseq.num].cmd = DTV_BANDWIDTH_HZ, p[cmdseq.num].u.data = parm.bandwidth, cmdseq.num++;
+#if not defined(__sh__)
 			if (system == SYS_DVBT2)
 			{
 				if (m_dvbversion >= DVB_VERSION(5, 3))
@@ -1934,6 +1937,7 @@ void eDVBFrontend::setFrontend(bool recvEvents)
 #endif
 				}
 			}
+#endif
 		}
 		else if (type == iDVBFrontend::feATSC)
 		{
@@ -2383,9 +2387,14 @@ int eDVBFrontend::isCompatibleWith(ePtr<iDVBFrontendParameters> &feparm)
 	else if (type == eDVBFrontend::feTerrestrial)
 	{
 		eDVBFrontendParametersTerrestrial parm;
+#if not defined(__sh__)
 		bool can_handle_dvbt, can_handle_dvbt2;
 		can_handle_dvbt = supportsDeliverySystem(SYS_DVBT, true);
 		can_handle_dvbt2 = supportsDeliverySystem(SYS_DVBT2, true);
+#else
+		bool can_handle_dvbt;
+		can_handle_dvbt = supportsDeliverySystem(SYS_DVBT, true);
+#endif
 		if (feparm->getDVBT(parm) < 0)
 		{
 			return 0;
@@ -2394,6 +2403,7 @@ int eDVBFrontend::isCompatibleWith(ePtr<iDVBFrontendParameters> &feparm)
 		{
 			return 0;
 		}
+#if not defined(__sh__)
 		if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !can_handle_dvbt2)
 		{
 			return 0;
@@ -2402,12 +2412,15 @@ int eDVBFrontend::isCompatibleWith(ePtr<iDVBFrontendParameters> &feparm)
 		{
 			return 0;
 		}
+#endif
 		score = 2;
+#if not defined(__sh__)
 		if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T && can_handle_dvbt2)
 		{
 			/* prefer to use a T tuner, try to keep T2 free for T2 transponders */
 			score--;
 		}
+#endif
 	}
 	else if (type == eDVBFrontend::feATSC)
 	{
