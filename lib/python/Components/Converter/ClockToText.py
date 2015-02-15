7578ac8dd3503dab3d82c5d81e66fa1786d7438c
Taapat <taapat@gmail.com>
2015-02-15 21:48:53 +0200

    Converter ClockToText: fix errors in some time formats, thx Mangust21 for report

--- lib/python/Components/Converter/ClockToText.py
+++ lib/python/Components/Converter/ClockToText.py
@@ -39,19 +39,19 @@ class ClockToText(Converter, object):
 			# TRANSLATORS: full time representation hour:minute:seconds
 			return _("%2d:%02d:%02d") % (t.tm_hour, t.tm_min, t.tm_sec)
 		elif self.type == "Date":
-			# TRANSLATORS: full date representation dayname daynum monthname year in strftime() format! See 'man strftime'
-			d = _(strftime("%A",t)) + " " + str(t[2]) + " " + MONTHS[t[1]-1] + " " + str(t[0])
+			# full date representation dayname daynum monthname year in strftime() format! See 'man strftime'
+			d = _(strftime("%A", t)) + " %e " + _(strftime("%B", t)) + " %Y" 
 		elif self.type == "Full":
-			# TRANSLATORS: long date representation short dayname daynum short monthname hour:minute in strftime() format! See 'man strftime'
-			d = dayOfWeek[t[6]] + _(" %d/%d  %2d:%02d") % (t[2],t[1], t.tm_hour, t.tm_min)
+			# long date representation short dayname daynum short monthname hour:minute in strftime() format! See 'man strftime'
+			d = _(strftime("%a", t)) + " %e/%m  %-H:%M"
 		elif self.type == "ShortDate":
-			# TRANSLATORS: short date representation short dayname daynum short monthname in strftime() format! See 'man strftime'
-			d = dayOfWeek[t[6]] + _(" %d/%d") % (t[2], t[1])
+			# short date representation short dayname daynum short monthname in strftime() format! See 'man strftime'
+			d = _(strftime("%a", t)) + " %e/%m"
 		elif self.type == "LongDate":
-			# TRANSLATORS: long date representations dayname daynum monthname in strftime() format! See 'man strftime'
-			d = dayOfWeek[t[6]] + " " + str(t[2]) + " " + MONTHS[t[1]-1]
+			# long date representations dayname daynum monthname in strftime() format! See 'man strftime'
+			d = _(strftime("%A", t)) + " %e " + _(strftime("%B", t))
 		elif self.type == "VFD":
-			# TRANSLATORS: VFD hour:minute in strftime() format! See 'man strftime'
+			# VFD hour:minute in strftime() format! See 'man strftime'
 			d = "%02d%02d" % (t.tm_hour, t.tm_min)
 		elif "Format" in self.type:
 			self.fmt_string = self.fmt_string.replace('%A',_(strftime("%A", t)))
@@ -60,7 +60,7 @@ class ClockToText(Converter, object):
 			self.fmt_string = self.fmt_string.replace('%b',_(strftime("%b", t)))
 			d = self.fmt_string
 		else:
-			# Default
+			# default
 			return _("%2d:%02d") % (t.tm_hour, t.tm_min)
 		return strftime(d, t)
 
