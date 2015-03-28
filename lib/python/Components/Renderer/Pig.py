e5a5aa503d9934561353c09bc660675aa8ff7f11
Taapat <taapat@gmail.com>
2015-03-28 14:20:56 +0200

    Renderer Pig: try fix unavailable services in channel selection

--- lib/python/Components/Renderer/Pig.py
+++ lib/python/Components/Renderer/Pig.py
@@ -9,7 +9,8 @@ class Pig(Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
 		self.Position = self.Size = None
-		self.hidePip = True
+		# fix unavailable services in channel selection with pig on spark
+		#self.hidePip = True
 
 	GUI_WIDGET = eVideoWidget
 
@@ -22,7 +23,7 @@ class Pig(Renderer):
 		attribs = self.skinAttributes[:]
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "hidePip":
-				self.hidePip = value == 1
+				#self.hidePip = value == 1
 				attribs.remove((attrib,value))
 		self.skinAttributes = attribs
 		ret = Renderer.applySkin(self, desktop, parent)
@@ -37,7 +38,7 @@ class Pig(Renderer):
 				self.instance.resize(self.Size)
 			if self.Position:
 				self.instance.move(self.Position)
-			self.hidePip and PipPigMode(True)
+			#self.hidePip and PipPigMode(True)
 
 	def onHide(self):
 		if self.instance:
