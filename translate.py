2286fa696a6a64c053d04b3dd6c5a9ee115ac3fb
Taapat <taapat@gmail.com>
2015-10-21 00:43:18 +0300

    Remove date translation from translate, now it is fixed with adding fakelocale

--- translate.py
+++ translate.py
@@ -1,32 +1,4 @@
 
-MONTHS = (
-	_("January"),
-	_("February"),
-	_("March"),
-	_("April"),
-	_("May"),
-	_("June"),
-	_("July"),
-	_("August"),
-	_("September"),
-	_("October"),
-	_("November"),
-	_("December"))
-
-shortMONTHS = (
-	_("Jan"),
-	_("Feb"),
-	_("Mar"),
-	_("Apr"),
-	_("May"),
-	_("Jun"),
-	_("Jul"),
-	_("Aug"),
-	_("Sep"),
-	_("Oct"),
-	_("Nov"),
-	_("Dec"))
-
 HOTKEY = (
 	_("Power"),
 	_("Scanning"),
