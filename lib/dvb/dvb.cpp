cdd88e83585ab828afe45b8b97f639e4f234cc17
schpuntik <schpuntik@freenet.de>
2016-03-28 19:49:33 +0200

    add  defined for DTV_ENUM_DELSYS

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -348,6 +348,7 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	struct dtv_properties props;
 	struct dtv_property prop[1];
 
+#if defined DTV_ENUM_DELSYS
 	prop[0].cmd = DTV_ENUM_DELSYS;
 	memset(prop[0].u.buffer.data, 0, sizeof(prop[0].u.buffer.data));
 	prop[0].u.buffer.len = 0;
@@ -356,7 +357,7 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 
 	if (ioctl(frontend, FE_GET_PROPERTY, &props) < 0)
 		eDebug("[eDVBUsbAdapter] FE_GET_PROPERTY DTV_ENUM_DELSYS failed %m");
-
+#endif
 	::close(frontend);
 	frontend = -1;
 
@@ -445,8 +446,10 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	ioctl(vtunerFd, VTUNER_SET_NAME, name);
 	ioctl(vtunerFd, VTUNER_SET_TYPE, type);
 	ioctl(vtunerFd, VTUNER_SET_FE_INFO, &fe_info);
+#if defined DTV_ENUM_DELSYS
 	if (prop[0].u.buffer.len > 0)
 		ioctl(vtunerFd, VTUNER_SET_DELSYS, prop[0].u.buffer.data);
+#endif
 	ioctl(vtunerFd, VTUNER_SET_HAS_OUTPUTS, "no");
 	ioctl(vtunerFd, VTUNER_SET_ADAPTER, nr);
 
