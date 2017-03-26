632d2798c6402379205e8b72467919bc179496e2
Taapat <taapat@gmail.com>
2017-03-26 20:59:46 +0300

    Standby: remove queue for delay for cec

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -270,13 +270,6 @@ class TryQuitMainloop(MessageBox):
 				if not inStandby:
 					if os.path.exists("/usr/script/standby_enter.sh"):
 						Console().ePopen("/usr/script/standby_enter.sh")
-					if SystemInfo["HasHDMI-CEC"] and config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
-						import Components.HdmiCec
-						Components.HdmiCec.hdmi_cec.secondBoxActive()
-						self.delay = eTimer()
-						self.delay.timeout.callback.append(self.quitMainloop)
-						self.delay.start(1500, True)
-						return
 			elif not inStandby:
 				config.misc.RestartUI.value = True
 				config.misc.RestartUI.save()
